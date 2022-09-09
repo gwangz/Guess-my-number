824f78bff03bcbb65ad407a97fc352a85b225e08
Gwangz <bnyagwanga@gmail.com>
2022-09-09 19:21:20 +0300

    DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+
+/*
+console.log (document.querySelector('.message').textContent)
+
+document.querySelector('.message').textContent = 'Yesss, Correct number'
+
+document.querySelector('.number').textContent = 13
+document.querySelector('.score').textContent = 10
+
+document.querySelector('.guess').value = 23
+
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+
+let score = 20
+
+document.querySelector('.number').textContent = secretNumber
+
+
+document.querySelector('.check').addEventListener('click', function(){
+const guess = Number(document.querySelector('.guess').value)
+console.log(guess, typeof guess)
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number';
+    }else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Yesss, Correct number';
+    }else if (guess > secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score 
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game terribly!';
+            document.querySelector('.score').textContent = 0
+        }
+    }else if (guess < secretNumber){
+        if (score > 1){
+            document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score 
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game terribly!';
+            document.querySelector('.score').textContent = 0
+        }
+
+    }
+})
